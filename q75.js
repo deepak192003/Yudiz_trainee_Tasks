e6c5503eb56a5f80f6067af67511023f0f9b5611
Deepak Kumar <sanisiwan@gmail.com>
2024-08-14 18:38:12 +0530

    new file added

---
+++ q75.js
@@ -0,0 +1,12 @@
+let array1 = [1,2,3];
+let array2 = [4,5,6];
+
+let newArr = [];
+
+newArr.push(array1[1]);
+newArr.push(array2[1]);
+
+for(let i = 0 ; i< newArr.length ; i++)
+{
+    console.log(newArr[i]);
+}
